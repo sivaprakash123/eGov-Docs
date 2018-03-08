2bbc9a13999d9088ca0a799346e2f247f0f0acc2
sivaprakash123 <sivaprakash.ramasamy@tarento.com>
2018-03-08 14:57:03 +0530

    updated Docker and entry point

--- docs/service-docs/entrypoint.sh
+++ docs/service-docs/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-cd /var/www/service-docs/
+cd /srv/jekyll/
 git clone https://github.com/sivaprakash123/eGov-Docs.git
 cd eGov-Docs/docs/service-docs
+chown -R jekyll:jekyll /srv/jekyll/eGov-Docs/docs/service-docs
 jekyll serve &
 nginx -g 'daemon off;'
